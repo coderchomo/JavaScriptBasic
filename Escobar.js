6c1822aff5d8facdc98da48e1ee02b2e60c39d66
coderchomo <codermermarket@gmail.com>
2021-08-17 15:03:07 +0700

    up file

---
+++ Escobar.js
@@ -0,0 +1,12 @@
+function Don(){
+    document.getElementById("1").innerHTML='Xin chào!';
+}
+function Pablo(){
+    alert('Xin chào');
+}
+function Pacho(){
+    document.write('Xin chào');
+}
+function LaQuica(){
+    console.log('Xin chào');
+}
